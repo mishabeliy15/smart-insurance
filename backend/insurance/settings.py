27436186025af9e2eb318823475b09d4ab84e9c4
Mikhail Beliy <misha.beliy15@gmail.com>
2020-12-01 18:44:47 +0200

    dockerize react app

--- backend/insurance/settings.py
+++ backend/insurance/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static-backend/"
 STATIC_ROOT = "static"
 
 # Media files
