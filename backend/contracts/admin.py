a283d2d5bb15ee44646943f0ef61e45f49350c01
Mikhail Beliy <misha.beliy15@gmail.com>
2020-12-01 22:41:39 +0200

    Add offer and contracts to django-admin

--- backend/contracts/admin.py
+++ backend/contracts/admin.py
@@ -1,3 +1,41 @@
+from contracts.models import Contract, Offer
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "company",
+        "customer",
+        "end_date",
+        "personal_coefficient",
+    )
+
+    fields = list_display
+    list_filter = (
+        "company",
+        "customer",
+        "end_date",
+    )
+    search_fields = list_filter
+
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "status",
+        "company",
+        "customer",
+        "months",
+        "personal_coefficient",
+    )
+
+    fields = list_display
+    list_filter = (
+        "company",
+        "customer",
+        "months",
+    )
+    search_fields = list_filter
